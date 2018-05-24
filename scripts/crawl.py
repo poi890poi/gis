Fix DEM sobel normalization; must combine x and y planes and apply vector normalization

scripts/crawl.py
@@ -12,18 +12,15 @@ working_dir = '../../data'
 sobelv = np.load(os.path.join(working_dir, 'sobelv.npy'))
 sobelh = np.load(os.path.join(working_dir, 'sobelh.npy'))
 
-# Normalize to [-1, 1]
-sobelv = (sobelv-0.5)*2
-sobelh = (sobelh-0.5)*2
-
 # Create slope map for visualization
 d_map = np.sqrt(sobelv*sobelv + sobelh*sobelh)
 d_preview = np.copy(d_map)
-normalize(d_preview)
+d_preview[:] = normalize(d_preview)
 fn_preview = os.path.join(working_dir, 'slope.png')
 imsave(fn_preview, (d_preview*255).astype(np.uint8))
 
 # Normalize vectors (keep direction and discard length)
+d_map[d_map==0] = 1.
 sobelv /= d_map
 sobelh /= d_map
 
@@ -47,14 +44,14 @@ increment = 1442695040888963407
 modulus = 2**64
 
 t = 0
-iterations = length//32
+iterations = length//8
 for i in range(iterations):
     if time() - t > 2:
         print('Pixels {} of {}, {}%'.format(i, iterations, i*100//iterations))
         t = time()
     p = index_to_yx(seed, height, width)
     #print('seed', seed, p)
-    for s in range(32):
+    for s in range(64):
         y = round(p[0])
         x = round(p[1])
         if y < 0 or y >= height or x < 0 or x >= width:
@@ -66,7 +63,7 @@ for i in range(iterations):
     seed = (multiplier*seed + increment) % (length + 1)
 
 # Save for preview
-normalize(trace_map)
+trace_map[:] = normalize(trace_map)
 rgb_map = np.ndarray(trace_map.shape + (3,), dtype=np.uint8)
 print(rgb_map.shape)
 rgb_map[:, :, 0] = (trace_map*255).astype(dtype=np.uint8)

scripts/filters.py
@@ -59,15 +59,17 @@ def process_save(source_array, kernel, source, prefix, band):
         # Convolve
         convolved = np.memmap(fn_full, dtype=np.float, mode='w+', shape=source_array.shape)
         kernel_combined = signal.convolve2d(kernel, ConvKernels.gaussian, mode='full')
-        convolved[:] = normalize(signal.convolve2d(source_array, kernel_combined, mode='same'))
+        convolved[:] = signal.convolve2d(source_array, kernel_combined, mode='same')
         convolved.flush()
         print('Convolved data saved', fn_full, sys.getsizeof(convolved))
         
-        # Save normalized small PNG for preview.
-        # Normalize is required after resizing because of resampling.
-        resized = normalize(imresize(convolved, (resize_target[0], resize_target[1])))
+        # Save resized array for proof of concept
+        # Do not normalize here; vector length normalization should be applied instead
+        resized = imresize(convolved, (resize_target[0], resize_target[1]))
         np.save(fn_small, resized)
-        preview = (resized*255.).astype(np.uint8)
+
+        # Save preview PNG
+        preview = (normalize(resized)*255.).astype(np.uint8)
         print(np.amin(resized), np.amax(resized), np.amin(preview), np.amax(preview))
         imsave(fn_preview, preview)
         print('Preview saved', fn_preview, resize_target)