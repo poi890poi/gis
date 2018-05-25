150ed6198af3564c251bb490430d61d0697f7c52
Lim Lee <poi890poi@gmail.com>
2018-05-26 00:41:32 +0800

    Take snapshot during crawling every T seconds, so result can be previewed before it's completed

--- scripts/crawl.py
+++ scripts/crawl.py
@@ -1,6 +1,7 @@
 import numpy as np
 from shared import normalize
-from scipy.misc import imsave
+from scipy.misc import imsave, imread
+from skimage.transform import resize as imresize
 import os.path
 import random
 from math import sqrt
@@ -10,20 +11,13 @@ from skimage.exposure import equalize_hist, adjust_gamma
 working_dir = '../../data'
 
 # Load vertical and horizontal gradient maps
-sobelv = np.load(os.path.join(working_dir, 'sobelv.npy'))
-sobelh = np.load(os.path.join(working_dir, 'sobelh.npy'))
+#sobelv = np.memmap(os.path.join(working_dir, 'sobelv.npy'), dtype=np.float, mode='r')
+#sobelh = np.memmap(os.path.join(working_dir, 'sobelh.npy'), dtype=np.float, mode='r')
+sobelv = np.load(os.path.join(working_dir, 'sobelv.npy'), mmap_mode='r')
+sobelh = np.load(os.path.join(working_dir, 'sobelh.npy'), mmap_mode='r')
 
-# Create slope map for visualization
-d_map = np.sqrt(sobelv*sobelv + sobelh*sobelh)
-d_preview = np.copy(d_map)
-d_preview[:] = (normalize(d_preview))
-fn_preview = os.path.join(working_dir, 'slope.png')
-imsave(fn_preview, (d_preview*255).astype(np.uint8))
-
-# Normalize vectors (keep direction and discard length)
-d_map[d_map==0] = 1.
-sobelv /= d_map
-sobelh /= d_map
+d_preview = imread(os.path.join(working_dir, 'sobelv.png'))
+print('d_preview', d_preview.shape, d_preview.dtype)
 
 def index_to_yx(index, rows, cols):
     y = index // cols
@@ -37,9 +31,19 @@ def format_time(t):
     t = int(t*1000)
     return '{}:{}:{}.{}'.format(str(t//3600000).zfill(2), str(t%3600000//60000).zfill(2), str(t%60000//1000).zfill(2), str(t%1000).zfill(3))
 
+def snapshot_preview(background, trace_map, preview_shape, working_dir):
+    # Save for preview
+    trace_ = normalize(imresize(trace_map, preview_shape))
+    rgb_map = np.ndarray(trace_.shape + (3,), dtype=np.uint8)
+    print('Snapshot for preview', rgb_map.shape)
+    rgb_map[:, :, 0] = (trace_*255).astype(dtype=np.uint8)
+    rgb_map[:, :, 1] = background
+    fn_preview = os.path.join(working_dir, 'trace.png')
+    imsave(fn_preview, rgb_map)    
+
 fn_trace = os.path.join(working_dir, 'trace.dat')
-trace_map = np.memmap(fn_trace, dtype=np.float, mode='w+', shape=d_map.shape)
-height, width, *_ = d_map.shape
+trace_map = np.memmap(fn_trace, dtype=np.float, mode='w+', shape=sobelv.shape)
+height, width, *_ = sobelv.shape
 length = height * width
 print('dimension', height, width, length)
 
@@ -50,14 +54,22 @@ modulus = 2 ** 64
 
 t_init = time()
 t = 0
+t_save = 0
 iterations = length // 4
 for i in range(iterations):
+
     if time() - t > 2:
         elapsed = time() - t_init
         ete = 'N/A'
         if i > 0: ete = format_time(elapsed * iterations / i)
-        print('Pixels {} of {}, {}%, elapsed: {}, ete: {}'.format(i, iterations, i*10000//iterations/100, format_time(elapsed), ete))
+        percent = i*10000//iterations/100
+        print('Pixels {} of {}, {}%, elapsed: {}, ete: {}'.format(i, iterations, percent, format_time(elapsed), ete))
         t = time()
+
+    if time() - t_save > 60:
+        snapshot_preview(d_preview, trace_map, trace_map.shape, working_dir)
+        t_save = time()
+
     p = index_to_yx(seed, height, width)
     y_ = -1
     x_ = -1
@@ -78,12 +90,3 @@ for i in range(iterations):
         x_ = x
         #print('move', v, v[0]*v[0]+v[1]*v[1], p)
     seed = (multiplier*seed + increment) % (length + 1)
-
-# Save for preview
-trace_map[:] = normalize(trace_map)
-rgb_map = np.ndarray(trace_map.shape + (3,), dtype=np.uint8)
-print(rgb_map.shape)
-rgb_map[:, :, 0] = (trace_map*255).astype(dtype=np.uint8)
-rgb_map[:, :, 1] = (d_preview*255).astype(dtype=np.uint8)
-fn_preview = os.path.join(working_dir, 'trace.png')
-imsave(fn_preview, rgb_map)
