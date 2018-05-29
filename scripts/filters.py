34f24bc6561cd72ce5dff8f3725c67955d6f19cb
Lim Lee <poi890poi@gmail.com>
2018-05-30 00:56:23 +0800

    Convert maps to uint8 and int8 for faster calculation

--- scripts/filters.py
+++ scripts/filters.py
@@ -44,17 +44,23 @@ class ConvKernels:
                         [ -2.0, -8.0,-12.0, -8.0, -2.0],
                         [ -1.0, -4.0, -6.0, -4.0, -1.0]])
 
+def convert_dtype(source_array, outpath, dtype):
+    cast_ = np.memmap(outpath, dtype=dtype, mode='w+', shape=source_array.shape)
+    cast_[:] = source_array.astype(dtype=dtype)
+    cast_.flush()
+    return cast_
+
 def save_preview(path, source_array, transform=None, target_shape=[4000, 4000]):
     if transform is not None:
-        lt = latlon_to_pixel(transform, (121.46883, 24.88643))
-        rb = latlon_to_pixel(transform, (121.68856, 24.68991))
+        lt = latlon_to_pixel(transform, (121., 25.))
+        rb = latlon_to_pixel(transform, (122., 24.))
         print('pixels', transform, lt, rb)
         roi = [min(lt[0], rb[0]), min(lt[1], rb[1]), max(lt[0], rb[0]), max(lt[1], rb[1])]
         print(roi)
         width = roi[2] - roi[0]        
         height = roi[3] - roi[1]        
 
-        resize = np.array(source_array[roi[1]:roi[1]+height, roi[0]:roi[0]+width])
+        resize = np.array(source_array[roi[1]:roi[1]+height, roi[0]:roi[0]+width]).astype(dtype=np.float)
 
         # Save preview PNG
         preview = (normalize(resize)*255.).astype(np.uint8)
@@ -77,6 +83,8 @@ def save_preview(path, source_array, transform=None, target_shape=[4000, 4000]):
         imsave(path, exposure.rescale_intensity(preview))
         print('Preview saved', path, target_shape)
 
+        del resize
+
 def process_save(source_array, kernel, source, prefix, band, transform):
     try:
         fn_full = prefix + '.dat'
@@ -93,6 +101,8 @@ def process_save(source_array, kernel, source, prefix, band, transform):
 
         fn_preview = prefix + '.png'
         save_preview(fn_preview, convolved, transform)
+
+        del convolved
 
     except:
         raise
@@ -134,50 +144,75 @@ lt = latlon_to_pixel(transform, lt)
 rb = latlon_to_pixel(transform, rb)
 print('pixels', transform, lt, rb)
 
-for i in range(1, bands + 1):
-    print()
-    print('Processing band {} of {}'.format(i, bands))
-    band_i = dem.GetRasterBand(i)
+print()
+# Process only first band
+i = 1
+print('Processing band {} of {}'.format(i, bands))
+band_i = dem.GetRasterBand(i)
 
-    raster = np.memmap(os.path.join(working_dir, 'dem.dat'), dtype=np.float, mode='w+', shape=(ysize, xsize))
-    raster[:] = band_i.ReadAsArray()
+raster = np.memmap(os.path.join(working_dir, 'dem-f64.dat'), dtype=np.float, mode='w+', shape=(ysize, xsize))
+raster[:] = band_i.ReadAsArray()
+print('Raster loaded', ysize, xsize, sys.getsizeof(raster))
 
-    fn_preview = os.path.join(working_dir, 'dem.png')
-    save_preview(fn_preview, raster, dem.GetGeoTransform())
+process_save(raster, ConvKernels.prom5, source, os.path.join(working_dir, 'prom-f64'), i, dem.GetGeoTransform())
+process_save(raster, ConvKernels.sobelv5, source, os.path.join(working_dir, 'sobelv-f64'), i, dem.GetGeoTransform())
+process_save(raster, ConvKernels.sobelh5, source, os.path.join(working_dir, 'sobelh-f64'), i, dem.GetGeoTransform())
 
-    print('Raster loaded', ysize, xsize, sys.getsizeof(raster))
-
-    # Kernel size of 3x3 is choosen because in 30m DEM it spans 90m and is already too rough for hiking
-    process_save(raster, ConvKernels.prom, source, os.path.join(working_dir, 'prom'), i, dem.GetGeoTransform())
-    #process_save(raster, ConvKernels.prom5, source, os.path.join(working_dir, 'prom5'), i)
-    process_save(raster, ConvKernels.sobelv, source, os.path.join(working_dir, 'sobelv'), i, dem.GetGeoTransform())
-    process_save(raster, ConvKernels.sobelh, source, os.path.join(working_dir, 'sobelh'), i, dem.GetGeoTransform())
-    #process_save(raster, ConvKernels.sobelv5, source, os.path.join(working_dir, 'sobelv5'), i)
-    #process_save(raster, ConvKernels.sobelh5, source, os.path.join(working_dir, 'sobelh5'), i)
+# Normalize elevation, convert to int8, save preview
+raster[:] = normalize(raster)
+raster *= 255
+dem_s = convert_dtype(raster, os.path.join(working_dir, 'dem.dat'), np.uint8)
+fn_preview = os.path.join(working_dir, 'dem.png')
+save_preview(fn_preview, dem_s, dem.GetGeoTransform())
+del dem_s
+del raster
 
 # Prepare vector length map and normalize sobel
-#sobelv = np.memmap(fn_full, dtype=np.float, mode='w+', shape=(1,)+source_array.shape+(1,))
-sobelv = np.memmap(os.path.join(working_dir, 'sobelv.dat'), dtype=np.float, mode='r+', shape=(ysize, xsize))
-sobelh = np.memmap(os.path.join(working_dir, 'sobelh.dat'), dtype=np.float, mode='r+', shape=(ysize, xsize))
+sobelv = np.memmap(os.path.join(working_dir, 'sobelv-f64.dat'), dtype=np.float, mode='r+', shape=(ysize, xsize))
+sobelh = np.memmap(os.path.join(working_dir, 'sobelh-f64.dat'), dtype=np.float, mode='r+', shape=(ysize, xsize))
 
-d_map = np.memmap(os.path.join(working_dir, 'vlen.dat'), dtype=np.float, mode='w+', shape=sobelv.shape)
+d_map = np.memmap(os.path.join(working_dir, 'vlen-f64.dat'), dtype=np.float, mode='w+', shape=sobelv.shape)
 d_map[:] = np.sqrt(sobelv*sobelv + sobelh*sobelh)
-
-# Save vector length as preview PNG
-fn_preview = os.path.join(working_dir, 'vlen.png')
-save_preview(fn_preview, d_map, dem.GetGeoTransform())
 
 # Normalize vectors (keep direction and discard length)
 d_map[d_map==0] = 1.
 sobelv /= d_map
 sobelh /= d_map
-sobelv.flush()
-sobelh.flush()
 
-fn_preview = os.path.join(working_dir, 'sobelv-n.png')
-save_preview(fn_preview, sobelv, dem.GetGeoTransform())
-fn_preview = os.path.join(working_dir, 'sobelh-n.png')
-save_preview(fn_preview, sobelh, dem.GetGeoTransform())
+# Normalize slope, convert to int8, save preview
+d_map[:] = normalize(d_map)
+d_map *= 255
+d_map_s = convert_dtype(d_map, os.path.join(working_dir, 'vlen.dat'), np.uint8)
+fn_preview = os.path.join(working_dir, 'vlen.png')
+save_preview(fn_preview, d_map_s, dem.GetGeoTransform())
+del d_map
+del d_map_s
+
+# Convert to int8
+sobelv *= 127
+sobelv_s = convert_dtype(sobelv, os.path.join(working_dir, 'sobelv.dat'), np.int8)
+sobelh *= 127
+sobelh_s = convert_dtype(sobelh, os.path.join(working_dir, 'sobelh.dat'), np.int8)
+
+# Save for preview
+fn_preview = os.path.join(working_dir, 'sobelv.png')
+save_preview(fn_preview, sobelv_s, dem.GetGeoTransform())
+fn_preview = os.path.join(working_dir, 'sobelh.png')
+save_preview(fn_preview, sobelh_s, dem.GetGeoTransform())
+del sobelv
+del sobelh
+del sobelv_s
+del sobelh_s
+
+# Normalize prominence, convert to int8, save preview
+prom = np.memmap(os.path.join(working_dir, 'prom-f64.dat'), dtype=np.float, mode='r+', shape=(ysize, xsize))
+prom[:] = normalize_symm(prom)
+prom *= 127
+prom_s = convert_dtype(prom, os.path.join(working_dir, 'prom.dat'), np.int8)
+fn_preview = os.path.join(working_dir, 'prom.png')
+save_preview(fn_preview, prom_s, dem.GetGeoTransform())
+del prom
+del prom_s
 
 dem = None
 
--- scripts/shared.py
+++ scripts/shared.py
@@ -2,24 +2,24 @@ import numpy as np
 
 def normalize(arr):
     print('normalize', arr.dtype)
-    min = np.amin(arr)
-    max = np.amax(arr)
+    min_ = np.amin(arr)
+    max_ = np.amax(arr)
 
     #hist, bin_edges = np.histogram(arr, 64)
     #print('hist', hist, bin_edges)
 
-    arr -= min
-    arr /= (max - min)
-    print(min, max, np.amin(arr), np.amax(arr))
+    arr -= min_
+    arr /= (max_ - min_)
+    print(min_, max_, np.amin(arr), np.amax(arr))
     return arr
 
 def normalize_symm(arr):
     print('symmetrical normalize', arr.dtype)
-    min = np.amin(arr)
-    max = np.amax(arr)
-    max = max(-min, max)
-    min = -max
-    arr -= min
-    arr /= (max - min)
-    print(min, max, np.amin(arr), np.amax(arr))
+    min_ = np.amin(arr)
+    max_ = np.amax(arr)
+    max_ = max(-min_, max_)
+    min_ = -max_
+    arr -= min_
+    arr /= (max_ - min_)
+    print(min_, max_, np.amin(arr), np.amax(arr))
     return arr
